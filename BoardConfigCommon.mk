b6b6ac5412018300a77bda21db15c7af44b7f781
Tim Zimmermann <tim@linux4.de>
2024-07-30 05:57:11 +0200

    universal9830: Switch to hardware/samsung FCM
    Change-Id: Id915542a0b5c4d27e488cae0d97ed36d299a428b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,6 +101,7 @@ TARGET_NO_RADIOIMAGE := true
 
 # Framework Matrix
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # HIDL Manifest
